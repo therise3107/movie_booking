8809ef19251dbf0da23c2126855df0a68fb61f41
therise3107 <lyadav3107@gmail.com>
2016-09-21 22:56:20 +0530

    migration serialized

--- db/migrate/20160921114258_create_bookings.rb
+++ db/migrate/20160921114258_create_bookings.rb
@@ -1,5 +1,13 @@
 class CreateBookings < ActiveRecord::Migration[5.0]
   def change
+  	create_table :shows do |t|
+      t.references :movie, foreign_key: true
+      t.references :theater, foreign_key: true
+      t.integer :bookings_count
+      t.datetime :start_time
+
+      t.timestamps
+    end  
     create_table :bookings do |t|
       t.references :user, foreign_key: true
       t.references :show, foreign_key: true
--- db/migrate/20160921143253_create_shows.rb
+++ db/migrate/20160921143253_create_shows.rb
@@ -1,12 +1,12 @@
-class CreateShows < ActiveRecord::Migration[5.0]
-  def change
-    create_table :shows do |t|
-      t.references :movie, foreign_key: true
-      t.references :theater, foreign_key: true
-      t.integer :bookings_count
-      t.datetime :start_time
+# class CreateShows < ActiveRecord::Migration[5.0]
+#   def change
+#     create_table :shows do |t|
+#       t.references :movie, foreign_key: true
+#       t.references :theater, foreign_key: true
+#       t.integer :bookings_count
+#       t.datetime :start_time
 
-      t.timestamps
-    end
-  end
-end
+#       t.timestamps
+#     end
+#   end
+# end
